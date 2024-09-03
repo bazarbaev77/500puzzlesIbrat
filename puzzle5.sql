8b60691688efe302e06f2cec7d06f92e8c4f1c00
bazarbaev77 <uzbibratl0@gmail.com>
2024-09-03 18:18:55 +0500

    file added

---
+++ puzzle5.sql
@@ -0,0 +1,28 @@
+CREATE TABLE PQ
+(
+ID INT
+,Name VARCHAR(10)
+,Typed VARCHAR(10)
+)
+ 
+
+INSERT INTO PQ(ID,Name,Typed) VALUES  (1,'P',NULL) , (1,NULL,'Q')
+
+SELECT * FROM  PQ
+
+
+;with cte_combined as (
+    select 
+        id,
+        max([name]) as [name],  
+        max(typed) as typed
+    from pq
+    group by id
+)
+select 
+    id,
+    [name],
+    typed
+from cte_combined
+where [name] is not null 
+and typed is not null
